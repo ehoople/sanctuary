f84ba244fd2021b0fb602c1a9a2d1253b921445c
Erin <hoople.erin@gmail.com>
2014-03-08 09:11:20 -0600

    enqueued jQuery, min.'s, and changed fonts from script to style

--- functions.php
+++ functions.php
@@ -82,19 +82,21 @@ function sanctuary_scripts() {
 	
 	wp_enqueue_style( 'sanctuary-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'jquery', false, false, false, false );
+	wp_enqueue_style( 'Arvo', 'http://fonts.googleapis.com/css?family=Arvo:400,700,700italic', false, false, false );
 
-	wp_enqueue_script( 'Arvo', 'http://fonts.googleapis.com/css?family=Arvo:400,700,700italic', false, false, false );
-
- 	wp_enqueue_script( 'Open Sans', 'http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700,300', false, false, false );
+ 	wp_enqueue_style( 'Open Sans', 'http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700,300', false, false, false );
 	
-	wp_enqueue_script( 'Open Sans Condensed', 'http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700', false, false, false );
+	wp_enqueue_style( 'Open Sans Condensed', 'http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700', false, false, false );
 
 	wp_enqueue_script( 'sanctuary-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'sanctuary-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	wp_enqueue_script( 'parallax', get_template_directory_uri() . '/js/parallax.js', array(), '1.0.0', true );
+
+	wp_enqueue_script( 'jquery-1.11.0.min', get_template_directory_uri() . '/js/jquery-1.11.0.min.js', array( 'jquery' ), false, true );
+
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', array(), '1.0.0', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
