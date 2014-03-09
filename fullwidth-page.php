cd8c681e3fcb7d5fd35b448281282b4ee3105473
Erin <hoople.erin@gmail.com>
2014-03-09 16:37:44 -0500

    updated background image

--- fullwidth-page.php
+++ fullwidth-page.php
@@ -9,11 +9,11 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 
-		<div id="first" class="slide" style="background-image: url('<?php the_field('background_slider1'); ?>');"></div>
-		<div id="second" class="slide" style="background-image: url('<?php the_field('background_slider2'); ?>');"></div>
-		<div id="third" class="slide" style="background-image: url('<?php the_field('background_slider3'); ?>');"></div>
-		<div id="fourth" class="slide" style="background-image: url('<?php the_field('background_slider4'); ?>');"></div>
-		<div id="fifth" class="slide" style="background-image: url('<?php the_field('background_slider5'); ?>');"></div>
+		<div id="first" class="full-width-slide" style="background-image: url('<?php the_field('background_slider1'); ?>');"></div>
+		<div id="second" class="full-width-slide" style="background-image: url('<?php the_field('background_slider2'); ?>');"></div>
+		<div id="third" class="full-width-slide" style="background-image: url('<?php the_field('background_slider3'); ?>');"></div>
+		<div id="fourth" class="full-width-slide" style="background-image: url('<?php the_field('background_slider4'); ?>');"></div>
+		<div id="fifth" class="full-width-slide" style="background-image: url('<?php the_field('background_slider5'); ?>');"></div>
 
 	</div><!-- #primary -->
 
